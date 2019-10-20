4ab68b587744507abb8a4e0aab73676c8bc67b3a
Eternlyours <56782955+Eternlyours@users.noreply.github.com>
2019-10-20 11:38:51 +0500

    Файлы для сайта

---
+++ js/script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() 
+{
+    $("#phone").mask("+7 (999) 99-99-999");
+});
+function show (inv)
+{
+   
+    var elem = document.getElementById(inv)
+    if (elem)
+        elem.style.display="block";      
+}
+function show (details)
+{
+    var elemm=document.getElementById(details);
+    if(elemm.style.display=="block")
+    {
+        elemm.style.display="none";
+    }
+    else
+    {
+        elemm.style.display="block";
+    }
+}
